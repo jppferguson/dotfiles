b39861501ae1a19909891d02175d48a2306c5ccc
Jamie Ferguson <jamie_ferguson@adnostic.co.uk>
2014-09-17 10:27:42 +0100

    Add 'gitgui' shortcut to open repo in a Sourcetree or some other Git GUI

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,6 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+# open with source tree
+alias sourcetree='open -a SourceTree'
--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,2 @@
 export EDITOR='subl'
+export GITGUI='SourceTree'
