4aa55b415adeed3c08d8e70af0314367cf7b3f00
Jamie Ferguson <jamie_ferguson@adnostic.co.uk>
2014-12-09 15:29:21 +0000

    Tidy up homebrew install/update scripts

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 #
 # Homebrew
-#
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
+#
+###############################################################################
+
+# Include the general functions
+. ./functions/general
+
+
+# No Homebrew :(
+print_block "Installing Homebrew"
 
 # Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
+if ! command_exists brew; then
+
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+
 else
-  echo "Homebrew already installed..."
+
+  # Let the user know what's happening
+  print_error "Homebrew already installed"
+
 fi
 
-echo "Installing homebrew packages."
+print_line "Installing Homebrew packages"
 
-# Install homebrew packages
+# Install Homebrew packages
 brew install grc coreutils spark nvm git-extras
 
-exit 0
+# And we're done
+print_block_end
--- homebrew/update.sh
+++ homebrew/update.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+#
 # Update homebrew
-set -e
+#
+###############################################################################
 
-echo "\nUpdating homebrew...";
+# Include the general functions
+. ./functions/general
+
+# Let the user know what's happening
+print_block "Updating Homebrew"
+
 brew update
+
+# And we're done
+print_block_end
