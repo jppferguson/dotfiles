027fc44514e35d93ef4f91705ad84627b9f134d6
Jamie Ferguson <jppferguson@gmail.com>
2015-01-12 23:57:29 +0000

    Fixup paths in sublime install script and add some output

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
+#
 # Setup a machine for Sublime Text 3
-set -x
+#
+###############################################################################
+
+# Include the general functions
+. ./functions/general
+
+print_block "Installing Sublime Text Preferences";
 
 # symlink settings in
-SUBLIME_DIR=~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-mv "$SUBLIME_DIR/User" "$SUBLIME_DIR/User.backup"
-ln -s "$DOTFILES/sublime/User" "$SUBLIME_DIR"
+SUBLIME_DIR="${HOME}/Library/Application\ Support/Sublime\ Text\ 3/Packages"
+
+# make sure file exists first
+mkdir -p "${SUBLIME_DIR}"
+
+# backup old settings folder
+mv "${SUBLIME_DIR}/User" "${SUBLIME_DIR}/User.backup"
+
+# symlink the User folder
+ln -s "${DOTFILES}/sublime/User" "${SUBLIME_DIR}"
+
+print_block_end
