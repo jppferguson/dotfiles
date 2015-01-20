bede0d73a6f122ce66b32255f984778a045625d2
Jamie Ferguson <jamie_ferguson@adnostic.co.uk>
2015-01-20 11:23:33 +0000

    Install oh-my-zsh brew-cask plugin

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -11,7 +11,7 @@ ZSH_THEME="agnoster"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(bower brew colorize composer git-flow git node npm osx ruby sublime)
+plugins=(bower brew brew-cask colorize composer git-flow git node npm osx ruby sublime)
 
 # disable oh-my-zsh updates entirely, only check when running `dot`
 DISABLE_AUTO_UPDATE=true
