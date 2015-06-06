9d62521ea41704b10f90acd907c26e9674f3c574
Jamie Ferguson <jppferguson@gmail.com>
2015-06-06 19:57:26 +0100

    Fix Sublime install to make it fully automated

--- sublime/install.sh
+++ sublime/install.sh
@@ -10,15 +10,22 @@
 print_block "Installing Sublime Text Preferences";
 
 # symlink settings in
-SUBLIME_DIR="${HOME}/Library/Application\ Support/Sublime\ Text\ 3/Packages"
+SUBLIME_DIR="${HOME}/Library/Application Support/Sublime Text 3"
+SUBLIME_PACKAGES_DIR="${SUBLIME_DIR}/Packages"
 
 # make sure file exists first
-mkdir -p "${SUBLIME_DIR}"
+mkdir -p "${SUBLIME_PACKAGES_DIR}"
 
 # backup old settings folder
-mv "${SUBLIME_DIR}/User" "${SUBLIME_DIR}/User.backup"
+if [ -f "${SUBLIME_PACKAGES_DIR}/User" ]; then
+	mv "${SUBLIME_PACKAGES_DIR}/User" "${SUBLIME_PACKAGES_DIR}/User.backup"
+fi
 
 # symlink the User folder
-ln -s "${DOTFILES}/sublime/User" "${SUBLIME_DIR}"
+ln -s "${DOTFILES}/sublime/User" "${SUBLIME_PACKAGES_DIR}"
+
+# Install package control
+mkdir -p "${SUBLIME_DIR}/Installed Packages/"
+ln -s "${DOTFILES}/sublime/Packages/Package Control.sublime-package" "${SUBLIME_DIR}/Installed Packages/Package Control.sublime-package"
 
 print_block_end
