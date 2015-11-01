56cb3c7dcc3b7d3e489aaf99ba15f866a7a499c8
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2015-11-01 23:55:29 +0000

    Add gulp to list of installed packages for oh-my-zsh

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -11,7 +11,7 @@ ZSH_THEME="agnoster"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(atom bower brew brew-cask colorize composer git-flow git node npm osx ruby sublime zsh_reload)
+plugins=(atom bower brew brew-cask colorize composer git-flow git gulp node npm osx ruby sublime zsh_reload)
 
 # disable oh-my-zsh updates entirely, only check when running `dot`
 DISABLE_AUTO_UPDATE=true
