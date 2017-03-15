6f584053a07cb8ffe9c98653bd3b0bd3892f108c
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2017-03-15 00:30:14 +0000

    Add some initial VS code settings

---
+++ vs-code/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Setup a machine for Visual Studio Code
+#
+###############################################################################
+
+# Include the general functions
+. ./functions/general
+
+print_block "Installing VS Code Preferences";
+
+# symlink settings in
+VS_CODE_DIR="${HOME}/Library/Application Support/Code"
+
+# make sure file exists first
+mkdir -p "${VS_CODE_DIR}"
+
+# backup old settings folder
+if [ -f "${VS_CODE_DIR}/User" ]; then
+	mv "${VS_CODE_DIR}/User" "${VS_CODE_DIR}/User.backup"
+fi
+
+# symlink the User folder
+ln -s "${DOTFILES}/vs-code/User" "${VS_CODE_DIR}"
+
+print_block_end
