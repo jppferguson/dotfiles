39132ee4bd1de34edc1e0ccd50369c60b3d89bba
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2017-10-21 14:02:29 +0100

    MacOS defaults updates

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -23,16 +23,20 @@ else
   exit 0
 fi
 
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
 # Ask for the administrator password upfront
 sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Set the default shell to zsh
 if [ "$(echo $SHELL)" != "/bin/zsh" ]; then
   sudo chsh -s /bin/zsh `whoami`
 fi
-
-# Keep-alive: update existing `sudo` time stamp until we're done
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # find all the .macos default files and then run them iteratively
 default_files=( $(find . -name "*.macos" ) )
