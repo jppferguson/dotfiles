26e698de9264aa43bbd3856a51dddf6d1f4722bc
Jamie Ferguson <git@jppferguson.com>
2022-07-17 23:56:21 +0100

    NVM Updates

--- node/env.zsh
+++ node/env.zsh
@@ -2,7 +2,8 @@ export NVM_DIR="$HOME/.nvm"
 
 if [[ -d $NVM_DIR ]]; then
 
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  source $(brew --prefix nvm)/nvm.sh
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 fi
