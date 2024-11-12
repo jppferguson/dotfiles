3bbdf2ac9ca104fb8115dbd30147c5d9dafedc63
Jamie Ferguson <git@jppferguson.com>
2024-11-12 17:39:31 +0000

    Update dock

--- src/topics/macos/set-dock.sh
+++ src/topics/macos/set-dock.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Toggl Track.app"
+dockutil --no-restart --add "/Applications/Mimestream.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Notion.app"
 dockutil --no-restart --add "/Applications/Notion Calendar.app"
-dockutil --no-restart --add "/Applications/iTerm2.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
