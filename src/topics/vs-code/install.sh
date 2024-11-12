c9d9adda5cc23308ad97873e02c80f791d0aa0aa
Jamie Ferguson <git@jppferguson.com>
2024-11-12 17:24:18 +0000

    Update VS code install script

--- src/topics/vs-code/install.sh
+++ src/topics/vs-code/install.sh
@@ -16,19 +16,24 @@ VS_CODE_INSIDERS_DIR="${HOME}/Library/Application Support/Code - Insiders"
 CUR_UNIX="`date +%s`"
 
 installVSCode () {
-	# make sure file exists first
-	mkdir -p "${1}"
-
+	print_task "Installing VSCode:" "${1}"
 	links=("User/settings.json" "User/keybindings.json")
 	for link in "${links[@]}"; do
+		FILE_DIR="${1}/$(dirname "${link}")"
+		# make sure directory exists first
+		mkdir -p "${FILE_DIR}"
+
 		# Backup
 		FILE_PATH="${1}/${link}"
 		FILE_BACKUP="${1}/_backup/${CUR_UNIX}/${link}"
 		if [[ -f "$FILE_PATH" ]]; then
-			mkdir -p "${FILE_BACKUP}"
+			print_line "Backing up existing:" "${link}"
+			FILE_BACKUP_DIR="$(dirname "${FILE_BACKUP}")"
+			mkdir -p "${FILE_BACKUP_DIR}"
 			mv "${1}/${link}" "${FILE_BACKUP}"
 		fi
 		# Link
+		print_line "Linking:" "${link}"
 		ln -s "${DOTFILES}/src/topics/vs-code/${link}" "${1}/${link}"
 	done
 }
