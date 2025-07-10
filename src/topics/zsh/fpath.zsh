fd0acb691e6d03655e8e9c6ccaef4da426c418b7
Jamie Ferguson <git@jppferguson.com>
2025-07-10 21:34:22 +0100

    Misc updates to asdf and topgrade

--- src/topics/zsh/fpath.zsh
+++ src/topics/zsh/fpath.zsh
@@ -1,2 +1,6 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+# Add each topic folder to fpath so that they can add functions and completion scripts
+for topic_folder ($DOTFILES_ZSH/*); do
+  if [ -d $topic_folder ]; then
+    fpath=($topic_folder $fpath)
+  fi
+done
